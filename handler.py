c7e073af56f3d2d95e45a9266fd5748b9cc0c87e
Mikko Seppälä <mikko.seppala@gofore.com>
2019-08-20 15:50:49 +0300

    python style improvements

--- handler.py
+++ handler.py
@@ -4,40 +4,45 @@ import uuid
 import os
 
 from botocore.exceptions import ClientError
-from decimal import Decimal
 
 dynamodb = boto3.resource('dynamodb')
 gameDb = dynamodb.Table(os.environ['DYNAMO_GAMES_TABLE'])
 
-def _getWsDetails(event):
-    req = event['requestContext']
-    connectionId = req['connectionId'];
-    endpointUrl = 'https://' + req['domainName'] + '/' + req['stage']
-    return connectionId, endpointUrl
 
-def _sendEventResponse(event, data):
-    connectionId, endpointUrl = _getWsDetails(event)
-    client = boto3.client('apigatewaymanagementapi', endpoint_url = endpointUrl)
+def _get_ws_details(event):
+    req = event['requestContext']
+    connection_id = req['connectionId'];
+    endpoint_url = 'https://' + req['domainName'] + '/' + req['stage']
+    return connection_id, endpoint_url
+
+
+def _send_event_response(event, data):
+    connection_id, endpoint_url = _get_ws_details(event)
+    client = boto3.client('apigatewaymanagementapi', endpoint_url = endpoint_url)
     client.post_to_connection(
-        ConnectionId = connectionId,
-        Data = json.dumps(data).encode('utf-8')
+        ConnectionId=connection_id,
+        Data=json.dumps(data).encode('utf-8')
     )
+
 
 def connect(event, context):
     return { 'statusCode': 200 }
 
+
 def disconnect(event, context):
     return { 'statusCode': 200 }
 
-def handleDefault(event, context):
+
+def handle_default(event, context):
     data = {'error': 'unknown action'}
-    _sendEventResponse(event, data)
+    _send_event_response(event, data)
 
     return { 'statusCode': 200 }
 
-def createGame(event, context):
 
-    connectionId, _ = _getWsDetails(event)
+def create_game(event, context):
+
+    connectionId, _ = _get_ws_details(event)
 
     newGame = {
       'players': {'1': connectionId, '2': None, '3': None, '4': None},
@@ -53,12 +58,11 @@ def createGame(event, context):
     }
 
     gameDb.put_item(Item=item)
-    _sendEventResponse(event, {'id': item['id']})
+    _send_event_response(event, {'id': item['id']})
     return { 'statusCode': 200 }
 
 
-
-def getGame(event, context):
+def get_game(event, context):
     body = json.loads(event['body'])
     id = body['id']
 
@@ -70,9 +74,9 @@ def getGame(event, context):
 
     print(item)
 
-    if(item):
-        _sendEventResponse(event, {'game': item})
+    if item:
+        _send_event_response(event, {'game': item})
     else:
-        _sendEventResponse(event, {'error': 'not found'})
+        _send_event_response(event, {'error': 'not found'})
 
     return { 'statusCode': 200 }
